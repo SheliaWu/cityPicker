5df64ba23363f1ea3443182927711b92deea46dd
SheliaWu <shelia.wu4575@gmail.com>
2017-09-24 13:17:26 +0800

    vue-resource to get local json data

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <cityPicker></cityPicker>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+import cityPicker from './components/cityPicker'
+
+export default {
+  name: 'app',
+  components: {
+    Hello,
+    cityPicker
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/cityPicker.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <select class="province">
+      <option v-for=""><option>
+    </select>
+    <select class="city">
+      <option v-for=""><option>
+    </select>
+    <select class="dist">
+      <option v-for=""><option>
+    </select>
+  </div>
+</template>
+
+<script>
+  export default{
+    name: 'cityPicker',
+    data () {
+      return{
+        citys:null
+      }
+    },
+    mounted() {
+      this.getData()
+    },
+    methods: {
+      getData:function() {
+        this.$http.get('../../static/city-data.json')
+          .then((response) => {
+            this.citys = response.body
+            console.log(this.citys)
+          }, response => {
+            console.log("error")
+          })
+      }
+    }
+  }
+</script>
